d0a108bdb0fd5770f11963a207b29467f5028740
Bernie Reilly <reilly_bernie@mac.com>
2024-01-03 21:52:02 -0500

    Format fixes

--- thermocouple/boot.py
+++ thermocouple/boot.py
@@ -8,19 +8,21 @@ import machine
 import micropython
 import network
 import esp
+
 esp.osdebug(None)
 import gc
+
 gc.collect()
 
-ssid = 'internets'
-password = 'reservoir'
-mqtt_server = '192.168.7.32'
-#EXAMPLE IP ADDRESS
-#mqtt_server = '192.168.1.144'
+ssid = "internets"
+password = "reservoir"
+mqtt_server = "192.168.7.32"
+# EXAMPLE IP ADDRESS
+# mqtt_server = '192.168.1.144'
 client_id = ubinascii.hexlify(machine.unique_id())
-topic_sub = b'notification'
-topic_pub = b'stove/stack/temp'
-topic_pub2 = b'stove/top/temp'
+topic_sub = b"notification"
+topic_pub = b"stove/stack/temp"
+topic_pub2 = b"stove/top/temp"
 
 last_message = 0
 message_interval = 5
@@ -32,7 +34,7 @@ station.active(True)
 station.connect(ssid, password)
 
 while station.isconnected() == False:
-  pass
+    pass
 
-print('Connection successful')
+print("Connection successful")
 print(station.ifconfig())
